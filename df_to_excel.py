546786ab8cb4a7c24165a36f7636d17ce8737fa2
youyoumubei <youyoumubei@126.com>
2023-08-22 20:13:52 +0800

    Update and rename df_to_excel to df_to_excel.py

--- df_to_excel.py
+++ df_to_excel.py
@@ -13,6 +13,43 @@ def write_data(args):
     df, sheet = args
     write_dataframe_to_sheet(df, sheet)
 
+from pymongo import MongoClient
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017')
+db = client['your_database']
+collection = db['your_collection']
+
+# Define the pipeline
+pipeline = [
+    # ... your aggregation stages ...
+]
+
+# Execute the pipeline and get a cursor
+cursor = collection.aggregate(pipeline)
+
+# Define a batch size
+batch_size = 1000
+
+# Generator function to yield batches of results
+def batched_results(cursor, batch_size):
+    batch = []
+    for doc in cursor:
+        batch.append(doc)
+        if len(batch) >= batch_size:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
+# Process the results in batches
+for result_batch in batched_results(cursor, batch_size):
+    # Process each document in the batch
+    for doc in result_batch:
+        # Process the document as needed
+        print(doc)
+
+
 if __name__ == "__main__":
     # Create some sample DataFrames
     data1 = {'A': [1, 2, 3], 'B': [4, 5, 6]}
